8d61cc8e7c3714b30cfa842c682e328783edee33
lucky <lucky@luckys-MacBook-Pro.local>
2020-11-26 14:06:33 +0800

    2020-11-26 14:06:30

---
+++ lib/imageSetsTest.dart
@@ -0,0 +1,99 @@
+import 'dart:ffi';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:newslash/httpUtil.dart';
+import 'package:newslash/snackBarUtil.dart';
+import 'package:newslash/enumUtil.dart' as EnumUtil;
+
+//7.网络
+void main() {
+  runApp(MaterialApp(
+    home: MyNetWorkBody(),
+  ));
+}
+
+class MyNetWorkBody extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MyNetWorkBodyState();
+}
+
+class MyNetWorkBodyState extends State<MyNetWorkBody> {
+  var imageSet = {
+    'pre': {
+      'image1': 'assets/images/b.jpg',
+      'image2': 'assets/images/c.jpg',
+      'image3': 'assets/images/a.jpg',
+    },
+    'normal': {
+      'image1': 'assets/images/a.jpg',
+      'image2': 'assets/images/b.jpg',
+      'image3': 'assets/images/c.jpg',
+    },
+    'next': {
+      'image1': 'assets/images/c.jpg',
+      'image2': 'assets/images/a.jpg',
+      'image3': 'assets/images/b.jpg',
+    },
+  };
+  var _nowShowWaht = 'normal';
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+        width: 300,
+        height: 400,
+        color: Colors.white,
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              MaterialButton(
+                onPressed: () {
+                  onNavigationPressed(context, 'before');
+                },
+                child: Icon(Icons.navigate_before),
+              ),
+              Expanded(
+                child: Image.asset(imageSet[_nowShowWaht]['image1']),
+              ),
+              Expanded(
+                flex: 3,
+                child: Image.asset(imageSet[_nowShowWaht]['image2']),
+              ),
+              Expanded(
+                flex: 1,
+                child: Image.asset(imageSet[_nowShowWaht]['image3']),
+              ),
+              MaterialButton(
+                onPressed: () {
+                  onNavigationPressed(context, 'next');
+                },
+                child: Icon(Icons.navigate_next),
+              ),
+            ],
+          ),
+        ));
+  }
+
+  onNavigationPressed(BuildContext context, String type) {
+    SnackBarUtil.showSimpleSnackBar(context, _nowShowWaht, 1500);
+    setState(() {
+      if (type == 'before' && _nowShowWaht == 'normal') {
+        _nowShowWaht = 'pre';
+      } else if (type == 'before' && _nowShowWaht == 'pre') {
+        _nowShowWaht = 'next';
+      } else if (type == 'before' && _nowShowWaht == 'next') {
+        _nowShowWaht = 'normal';
+      } else if (type == 'next' && _nowShowWaht == 'normal') {
+        _nowShowWaht = 'next';
+      } else if (type == 'next' && _nowShowWaht == 'pre') {
+        _nowShowWaht = 'normal';
+      } else if (type == 'next' && _nowShowWaht == 'next') {
+        _nowShowWaht = 'normal';
+      }
+    });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,51 @@ import 'package:flutter/material.dart';
 import 'package:newslash/httpUtil.dart';
 import 'package:newslash/snackBarUtil.dart';
 import 'package:newslash/enumUtil.dart' as EnumUtil;
+import 'package:newslash/widgetTest/listAndGridDemo.dart';
+import 'package:newslash/widgetTest/list_demo.dart';
 
-//7.网络
+//3.ListView 1
 void main() {
-  runApp(MaterialApp(
-    home: MyNetWorkBody(),
-  ));
+  runApp(ListViewTestApp());
 }
+
+class ListViewTestApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => GridViewTestAppState();
+}
+
+class ListViewTestAppState extends State<StatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return ListDemoWidget();
+  }
+}
+
+//2、GridView 1
+// void main() {
+//   runApp(GridViewTestApp());
+// }
+
+class GridViewTestApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => GridViewTestAppState();
+}
+
+class GridViewTestAppState extends State<StatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return ListAndGridDemoApp();
+  }
+}
+
+//1、Container 实例
+// void main() {
+//   runApp(MaterialApp(
+//     home: MyNetWorkBody(),
+//   ));
+// }
 
 class MyNetWorkBody extends StatefulWidget {
   @override
@@ -19,26 +57,38 @@ class MyNetWorkBody extends StatefulWidget {
 }
 
 class MyNetWorkBodyState extends State<MyNetWorkBody> {
-  var _netResponJsonData = '';
+  Widget _buildImageColumn() => Container(
+        decoration: BoxDecoration(
+          color: Colors.black26,
+        ),
+        child: Column(
+          children: [
+            _buildImageRow(1),
+            _buildImageRow(3),
+          ],
+        ),
+      );
+  Widget _buildDecoratedImage(int imageIndex) => Expanded(
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(width: 10, color: Colors.white),
+            borderRadius: const BorderRadius.all(const Radius.circular(8)),
+          ),
+          margin: const EdgeInsets.all(4),
+          child: Image.asset('assets/images/pic$imageIndex.jpg',
+              fit: BoxFit.fitHeight),
+        ),
+      );
+
+  Widget _buildImageRow(int imageIndex) => Row(
+        children: [
+          _buildDecoratedImage(imageIndex),
+          _buildDecoratedImage(imageIndex + 1),
+        ],
+      );
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text(_netResponJsonData),
-          Expanded(
-            child: Image.asset('assets/images/a.jpg'),
-          ),
-          Expanded(
-            child: Image.asset('assets/images/b.jpg'),
-          ),
-          Expanded(
-            child: Image.asset('assets/images/c.jpg'),
-          ),
-        ],
-      ),
-    );
+    return _buildImageColumn();
   }
 }
