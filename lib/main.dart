0bd437ea7f74ccd5ae4e2b64430d7479cd150f09
AndroidIosWebEmbeddedFirmwareDver <494951601@qq.com>
2020-12-02 17:28:06 +0800

    2020-12-02-17-28-06

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:newslash/utils/httpUtil.dart';
 import 'package:newslash/utils/snackBarUtil.dart';
 import 'package:newslash/utils/enumUtil.dart' as EnumUtil;
-import 'package:newslash/widgetTest/listAndGridDemo.dart';
-import 'package:newslash/widgetTest/list_demo.dart';
 
 import 'autoreject/auto_reject_app.dart';
 import 'autoreject/isolate_demo_app.dart';
@@ -18,89 +16,4 @@ void main() {
     // home: IsolateDemoSampleApp(),
     home: AutoRejectApp(),
   ));
-}
-
-//3.ListView 1
-// void main() {
-//   runApp(ListViewTestApp());
-// }
-
-// class ListViewTestApp extends StatefulWidget {
-//   @override
-//   State<StatefulWidget> createState() => GridViewTestAppState();
-// }
-
-// class ListViewTestAppState extends State<StatefulWidget> {
-//   @override
-//   Widget build(BuildContext context) {
-//     // TODO: implement build
-//     return ListDemoWidget();
-//   }
-// }
-
-//2、GridView 1
-// void main() {
-//   runApp(GridViewTestApp());
-// }
-
-class GridViewTestApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => GridViewTestAppState();
-}
-
-class GridViewTestAppState extends State<StatefulWidget> {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return ListAndGridDemoApp();
-  }
-}
-
-//1、Container 实例
-// void main() {
-//   runApp(MaterialApp(
-//     home: MyNetWorkBody(),
-//   ));
-// }
-
-class MyNetWorkBody extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => MyNetWorkBodyState();
-}
-
-class MyNetWorkBodyState extends State<MyNetWorkBody> {
-  Widget _buildImageColumn() => Container(
-        decoration: BoxDecoration(
-          color: Colors.black26,
-        ),
-        child: Column(
-          children: [
-            _buildImageRow(1),
-            _buildImageRow(3),
-          ],
-        ),
-      );
-  Widget _buildDecoratedImage(int imageIndex) => Expanded(
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(width: 10, color: Colors.white),
-            borderRadius: const BorderRadius.all(const Radius.circular(8)),
-          ),
-          margin: const EdgeInsets.all(4),
-          child: Image.asset('assets/images/pic$imageIndex.jpg',
-              fit: BoxFit.fitHeight),
-        ),
-      );
-
-  Widget _buildImageRow(int imageIndex) => Row(
-        children: [
-          _buildDecoratedImage(imageIndex),
-          _buildDecoratedImage(imageIndex + 1),
-        ],
-      );
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return _buildImageColumn();
-  }
 }
